16f03953091dc2572e85d210b9a24c2a3b54e700
Drausio <drausiodonini@gmail.com>
2018-12-09 13:39:26 -0500

    I changed the name of the controller created in the creation  of the project. I inserted the connection string in the appsettings.json I setup the references of the Entity Framework in the project

--- Controllers/ToDoController.cs
+++ Controllers/ToDoController.cs
@@ -8,35 +8,35 @@ namespace ToDoCRUDRestApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class ToDoController : ControllerBase
     {
-        // GET api/values
+        // GET api/ToDo
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET api/ToDo/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST api/ToDo
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/ToDo/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/ToDo/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
